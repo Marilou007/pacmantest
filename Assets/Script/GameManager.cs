1ba2dae60bee3c20da04e1b90246138f67941f51
cath-lariviere <105387388+cath-lariviere@users.noreply.github.com>
2023-01-20 14:01:37 -0500

    GameManager
    Programmation de base du game manager

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //-------------------------------------------variables-----------------------------------------
+   //reference fantomes
+    public Fantome[ ] fantomes;
+    //reference pacman
+    public Pacman pacman;
+    //reference palette
+    public Transform palette;
+    //reference score
+    public int score { get; private set; }
+    //reference vies
+    public int vies { get; private set; }
+
+    //-----------------------------------------function-------------------------------------
+
+    //----------------------function general
+    //debut partie
+    private void Start()
+    {
+        NewGame();
+    }
+    //reinitialisé pour une nouvelle partie
+    private void NewGame()
+    {
+        SetScore(0);
+        Setlives(3);
+        NewRound();
+    }
+
+    //reinitialisé pour une nouvelle round
+    private void NewRound()
+    {
+        foreach( Transform palette in this.palette)
+        {
+            palette.gameObject.SetActive(true);
+        }
+
+        ResetState();
+    }
+    //réinitalisé quand pacman se fait manger
+    private void ResetState()
+    {
+        for (int i = 0; i < this.fantomes.Length; i++)
+        {
+            this.fantomes[i].gameObject.SetActive(true);
+        }
+
+        this.pacman.gameObject.SetActive(true);
+    }
+    //quand la partie est terminé
+    private void Gameover()
+    {
+        for (int i = 0; i < this.fantomes.Length; i++)
+        {
+            this.fantomes[i].gameObject.SetActive(false);
+        }
+
+        this.pacman.gameObject.SetActive(false);
+    }
+
+    //----------------------------------------- function specifique------------------------------
+    //mettre le score
+    private void SetScore(int score)
+    {
+        this.score = score;
+    }
+    //mettre les vies
+    private void Setlives(int vies)
+    {
+        this.vies = vies;
+    }
+
+    //----fonction publique
+    //quand pacman mange un fantome
+    public void FantomeManger(Fantome fantome)
+    {
+        SetScore(this.score + fantome.points);
+    }
+    //quand un fantome mange pacman
+    public void PacmanManger()
+    {
+        this.pacman.gameObject.SetActive(false);
+
+        Setlives(this.vies -1);
+
+        if(this.vies > 0)
+        {
+            Invoke(nameof(ResetState), 3.0f);
+        }
+
+        else
+        {
+            Gameover();
+        }
+    }
+}
