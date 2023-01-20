47ec40ce65227e9fb33a667b48779255e03f44f4
cath-lariviere <105387388+cath-lariviere@users.noreply.github.com>
2023-01-20 16:56:04 -0500

    Mouvement pacman
    les inputs du joueur fait bouger pacman dans la bonne direction + pacman fait uen rotation pour aller dans la bonne direction

--- Assets/Script/Pacman.cs
+++ Assets/Script/Pacman.cs
@@ -2,17 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Mouvement))]
 public class Pacman : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //-------------------------------------------variables-----------------------------------------
+    public Mouvement mouvement { get; private set; }
+
+    //-----------------------------------------function-------------------------------------
+
+    //----------------------function general
+
+    private void Awake()
     {
-        
+        this.mouvement = GetComponent<Mouvement>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.mouvement.setDirection(Vector2.up);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.mouvement.setDirection(Vector2.down);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.mouvement.setDirection(Vector2.left);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.mouvement.setDirection(Vector2.right);
+        }
+
+        float angle = Mathf.Atan2(this.mouvement.direction.y, this.mouvement.direction.x);
+        this.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
     }
+
+    //----------------------------------------- function specifique------------------------------
 }
