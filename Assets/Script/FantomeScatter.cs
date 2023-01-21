c08248efd2018f8db370d90fa5394c97168dc643
cath-lariviere <105387388+cath-lariviere@users.noreply.github.com>
2023-01-21 12:48:15 -0500

    scatter + node correction
    Le comportement scatter a été implémenter + le layermask a été indiqué pour le node

--- Assets/Script/FantomeScatter.cs
+++ Assets/Script/FantomeScatter.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class FantomeScatter : FantomeBehavior
 {
-   
+    //-----------------------------------------function-------------------------------------
+
+    //----------------------function general
+    private void OnTriggerEnter2D(Collider2D autre)
+    {
+        Node node = autre.GetComponent<Node>();
+
+        if(node != null && this.enabled && !this.fantome.peur.enabled)
+        {
+            int index = Random.Range(0, node.DirectionDisponible.Count);
+
+            if (node.DirectionDisponible[index] == -this.fantome.mouvement.direction && node.DirectionDisponible.Count > 1)
+            {
+                index++;
+
+                if(index >= node.DirectionDisponible.Count)
+                {
+                    index = 0;
+                }
+            }
+
+            this.fantome.mouvement.setDirection(node.DirectionDisponible[index]);
+        }
+    }
+
+    private void OnDisable()
+    {
+        this.fantome.poursuite.Enable();
+    }
+
+    
 }
